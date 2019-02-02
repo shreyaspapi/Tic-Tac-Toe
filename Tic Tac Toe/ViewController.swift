715ec62a47ac6a47eb2471b9b3cf238ceab8d893
shreyaspapi <spapinwar@gmail.com>
2019-02-03 01:03:22 +0530

    Clearing constraints

--- Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var player1Label: UILabel!
     @IBOutlet weak var player2Label: UILabel!
     
-    
+    let winningPositions = [[0,1,2], [3,4,5], [6,7,8], [0, 3, 6], [1,4,7], [2,5,8], [], []]
     
     var count = 0
     
